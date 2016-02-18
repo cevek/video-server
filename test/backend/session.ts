e004e8f68cca4b9af9994d681ed8fcca5ceb8381
cody <cevek@ya.ru>
2016-02-19 00:14:54 +0500

    B: Fixed thumbnail generating

--- test/backend/session.ts
+++ test/backend/session.ts
@@ -238,7 +238,8 @@ export class Session {
         console.log("ExtractThumbs");
         var fld = this.folder;
         await exec(`ffmpeg -y -i ${this.inputFile} -qscale 1 -vsync 1 -r 1 ${fld}_raw%03d.jpg`);
-        await exec(`convert ${fld}_raw*.jpg -thumbnail 200x100^ -auto-level -level 0,60%% -modulate 100,70 ${fld}_thumb.jpg`);
+        await exec(`convert ${fld}_raw*.jpg -gravity center -thumbnail 200x100^ -extent 200x100 ${fld}_thumb.jpg`);
+        // await exec(`convert ${fld}_raw*.jpg -gravity center -thumbnail 200x100^ -extent 200x100 -auto-level -level 0,60%% -modulate 100,70 ${fld}_thumb.jpg`);
         await exec(`montage ${fld}_thumb*.jpg  -tile 20x -geometry +0+0 -gravity north ${fld}thumbs.jpg`);
         await exec(`rm ${fld}_*.jpg`);
     }
