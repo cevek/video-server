ce5b23e684ae65873d179885896627e8b115712f
cody <cevek@ya.ru>
2016-02-20 23:27:54 +0500

    F: Fixed spectrogram

--- test/frontend/src/viewer/viewer.tsx
+++ test/frontend/src/viewer/viewer.tsx
@@ -11,6 +11,7 @@ import {svgPathGenerator} from "../utils/svg-path-generator";
 import {ITextLine} from "../../../interfaces/text-line";
 import {AudioSelection} from "./audio-selection";
 
+import {FFT} from "sound-utils/FFT";
 import {SoundLoader} from "sound-utils/SoundLoader";
 import {Play} from "sound-utils/Play";
 import {Spectrogram} from "sound-utils/Spectrogram";
@@ -26,9 +27,9 @@ export class Viewer extends React.Component<{params: any, resolved: PostModel}, 
     isEnded = false;
     isPlaying = false;
     player = new Play(audioContext);
-    videoPlayer: any;
+    videoPlayer:any;
     soundLoaded = false;
-    spectrogram = new Spectrogram(128);
+    spectrogram = new Spectrogram(256);
 
     static load(params:any) {
         return PostModel.fetch(params.id);
@@ -74,23 +75,81 @@ export class Viewer extends React.Component<{params: any, resolved: PostModel}, 
         });
     }
 
+    spectrogramData:Uint8ClampedArray[];
+    audioBuffer: AudioBuffer;
+
     loadSound() {
         var data = this.props.resolved.data;
         const url = config.baseUrl + '/' + data.mediaFiles[data.post.enAudio].url;
         new SoundLoader(audioContext).fromUrl(url).then(audioBuffer => {
+            this.audioBuffer = audioBuffer;
+            this.duration = audioBuffer.duration;
             this.player.setAudio(audioBuffer);
-            this.spectrogram.process(audioBuffer);
+            this.spectrogramData = this.process(audioBuffer, 64);
+            // this.spectrogram.process(audioBuffer);
             this.loadSpectrogram();
             this.soundLoaded = true;
             this.forceUpdate();
         });
     }
 
-    loadSpectrogram(){
-        const imdata = this.spectrogram.getImageData();
+
+    loadSpectrogram() {
+        const data = this.spectrogramData;
         const canvas = React.findDOMNode(this.refs['spectrogram']) as HTMLCanvasElement;
         const ctx = canvas.getContext('2d');
-        ctx.putImageData(imdata,0, 0, 0, 0, canvas.width, canvas.height);
+
+
+        var timeLen = data.length;
+        var valsLen = data[0].length;
+        canvas.width = valsLen;
+        canvas.height = timeLen;
+
+        var imd = new ImageData(valsLen, timeLen);
+        var imdd = imd.data;
+        for (var i = 0; i < timeLen; i++) {
+            var vals = data[i];
+            for (var j = 0; j < valsLen; j++) {
+                var val = 255 - vals[j] * 5;
+                var pos = (i * valsLen + valsLen - j) * 4;
+                imdd[pos + 0] = val;
+                imdd[pos + 1] = val;
+                imdd[pos + 2] = val;
+                imdd[pos + 3] = 255;
+            }
+        }
+        ctx.putImageData(imd, 0, 0);
+        return imd;
+    }
+
+    process(audioBuffer:AudioBuffer, fftSize:number) {
+        const data:Uint8ClampedArray[] = [];
+        const koef = 30;
+        var bufferSize = fftSize;
+        var bufferDataSize = fftSize * koef;
+        var fft = new FFT(bufferSize, 0);
+        var signal:Float32Array = audioBuffer.getChannelData(0);
+        var bufferSignal = new Float32Array(bufferSize);
+        var k = 0;
+        while (signal.length > k + bufferDataSize) {
+            const bb = signal.subarray(k, k + bufferDataSize);
+            const kk = new Float32Array(bufferSize);
+            for (var i = 0; i < bb.length; i += koef) {
+                kk[i / koef | 0] = bb[i];
+            }
+            bufferSignal.set(kk);
+            k += bufferDataSize;
+            fft.forward(bufferSignal);
+            var spectrum = fft.spectrum;
+            var arr = new Uint8ClampedArray(spectrum.length);
+            for (var j = 0; j < spectrum.length; j++) {
+                // equalize, attenuates low freqs and boosts highs
+                //arr[j] = spectrum[j] * -1 * Math.log((bufferSize / 2 - j) * (0.5 / bufferSize / 2)) * bufferSize | 0;
+                arr[j] = spectrum[j] * 5000;
+            }
+            data.push(arr);
+        }
+        return data;
     }
 
     componentDidMount() {
@@ -116,16 +175,16 @@ export class Viewer extends React.Component<{params: any, resolved: PostModel}, 
         const connectorWidth = 50;
         const halfLineH = lineH / 2;
 
+        const durationY = this.duration * 100 / resizeKoef;
+
         const positions = lines.map(line => (line.en.start + line.en.dur / 2) / resizeKoef);
         const renderLines = new LineAllocator(positions, 50).allocateRenderLines();
         const svgWidth = 100;
-        const svgHeight = Math.max(positions[positions.length - 1] + 100, renderLines[renderLines.length - 1] + halfLineH);
-        const duration = data.mediaFiles[data.post.video].duration;
-        const durationY = duration * 100 / resizeKoef;
+        const svgHeight = durationY;
         console.log(durationY);
 
 
-        const thumbImg = `http://localhost:1335/` + data.mediaFiles[data.post.thumbs].url;
+        const thumbImg = config.baseUrl + '/' + data.mediaFiles[data.post.thumbs].url;
         const thumbWidth = 200;
         const thumbHeight = 100;
         const thumbsPerLine = 20;
@@ -133,7 +192,7 @@ export class Viewer extends React.Component<{params: any, resolved: PostModel}, 
         const thumbsCount = durationY / thumbHeight | 0;
         const thumbsItems:{top:number;imgTop:number;imgLeft:number}[] = [];
         for (var i = 0; i < thumbsCount; i++) {
-            const k = Math.round(i * thumbHeight / durationY * duration);
+            const k = Math.round(i * thumbHeight / durationY * this.duration);
             thumbsItems.push({
                 top: i * thumbHeight + thumbShift,
                 imgTop: (k / thumbsPerLine | 0) * thumbHeight,
@@ -143,7 +202,7 @@ export class Viewer extends React.Component<{params: any, resolved: PostModel}, 
         return <div>
             {this.currentTime}
             <div className="timeline" ref="timeline">
-                <canvas className="spectrogram" ref="spectrogram" width={durationY} height={50}/>
+                <canvas className="spectrogram" ref="spectrogram" style={{height: durationY}}/>
                 <svg className="timeline-connector" width={svgWidth} height={svgHeight}>
                     {renderLines.map((pos, i) => {
                         const textLine = lines[i].en;
@@ -155,11 +214,12 @@ export class Viewer extends React.Component<{params: any, resolved: PostModel}, 
                         return  <path fill={color} d={svgPathGenerator(tl, bl, tr, br, connectorWidth)}/>
                         })}
                 </svg>
-                <AudioSelection pxPerSec={100 / resizeKoef} player={this.player} duration={duration}/>
+                <AudioSelection pxPerSec={100 / resizeKoef} player={this.player} duration={this.duration}/>
             </div>
             <div className="thumbs">
-                {thumbsItems.map(thumb => <div className="thumb"
-                                               style={{top: thumb.top, background: `url(${thumbImg}) ${-thumb.imgLeft}px ${-thumb.imgTop}px`}}>
+                {thumbsItems.map(thumb =>
+                <div className="thumb"
+                     style={{top: thumb.top, background: `url(${thumbImg}) ${-thumb.imgLeft}px ${-thumb.imgTop}px`}}>
                 </div>)}
             </div>
             <div ref={d => this.videoWrapper = React.findDOMNode(d)} className="video">
