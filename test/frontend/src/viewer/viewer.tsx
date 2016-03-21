8dc40002f3f35962474837fef5cbd0d49f5f5bb1
cody <cevek@ya.ru>
2016-03-21 23:29:29 +0500

    F: added toolbar

--- test/frontend/src/viewer/viewer.tsx
+++ test/frontend/src/viewer/viewer.tsx
@@ -12,13 +12,14 @@ import {EditorHistory} from "../utils/history";
 
 export class Viewer extends React.Component<{params: any, resolved: PostModel}, {}> {
     audioPlayer = new AudioPlayer();
-    history: EditorHistory;
+    history = new EditorHistory();
 
     static load(params:any) {
         return PostModel.fetch(params.id);
     }
 
     componentDidMount() {
+        (window as any).editorHistory = this.history;
         var data = this.props.resolved.data;
         const enAudio = data.mediaFiles[data.post.enAudio];
         const url = config.baseUrl + '/' + enAudio.url;
@@ -37,11 +38,16 @@ export class Viewer extends React.Component<{params: any, resolved: PostModel}, 
         const renderLines = new LineAllocator(positions, 50).allocateRenderLines();
 
         return <div>
+            <div className="toolbar">
+                <button onClick={()=>this.history.undo()}>Undo</button>
+                <button onClick={()=>this.history.redo()}>Redo</button>
+            </div>
             {this.audioPlayer.soundLoaded ?
                 <div>
                     <Timeline postModel={postModel} resizeKoef={resizeKoef} player={this.audioPlayer}/>
                     <TimelineConnector postModel={postModel} lineH={lineH} resizeKoef={resizeKoef}
                                        player={this.audioPlayer}
+                                       history={this.history}
                                        renderLines={renderLines}/>
                 </div> : null
             }
