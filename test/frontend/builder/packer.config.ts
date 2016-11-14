00148dc9ac8368502db304a2d8705a8d50239764
cevek <cevek@ya.ru>
2016-11-14 20:23:37 +0300

    Use new version webbuilder

--- test/frontend/builder/packer.config.ts
+++ test/frontend/builder/packer.config.ts
@@ -1,52 +1,21 @@
-import {Packer} from "packer";
-import {ts} from "packer/dist/plugins/ts";
-import {combineJS} from "packer/dist/plugins/combineJS";
-import {dest} from "packer/dist/plugins/dest";
-import {jsEntry} from "packer/dist/plugins/jsEntry";
-import {copy} from "packer/dist/plugins/copy";
-import {sass} from "packer/dist/plugins/sass";
-import {combineCSS} from "packer/dist/plugins/combineCSS";
-import * as path from "path";
+import {Packer, copy, combineJS, combineCSS} from "webbuilder";
+import {ts} from "webbuilder/dist/plugins/ts";
+import {sass} from "webbuilder/dist/plugins/sass";
+import * as Koa from "koa";
 Error.stackTraceLimit = Infinity;
 
 new Packer({dest: 'dist', context: __dirname + '/../src/'}, promise => promise
-        .then(ts())
-        .then(jsEntry('app.js'))
-        // .then(hmr())
-        // .then(copy('test/*.js'))
-        // .then(jsEntry('test/r.js'))
-        .then(copy('index.html'))
-        // .then(sass('index.scss'))
-        .then(combineJS('bundle.js'))
-        .then(sass(''))
-        .then(combineCSS('style.css'))
-        .then(dest())
-    /*
-     .then(postcss())
-     .then(combineJS('dist/vendor.js', ['react', 'mobx']))
-     .then(combineCSS('dist/style.css'))
-     .then(hash())
-     .then(jade())
-     .then(dest())*/
+    .then(ts())
+    .then(sass())
+    .then(copy('index.html'))
+    .then(combineJS('app.js', 'bundle.js'))
+    .then(combineCSS('style.css'))
 ).watch(() => {
 
 });
 
-import * as Koa from 'koa';
-
-// var koa = require('koa');
-var serve = require('koa-static');
-
-
+const serve = require('koa-static');
 const app = new Koa();
 app.use(serve(__dirname + '/../src/dist/'));
 app.listen(3000);
-
-const unhandledRejections = new Map();
-process.on('unhandledRejection', (reason: any, p: any) => {
-    console.error(reason.stack);
-    unhandledRejections.set(p, reason);
-});
-process.on('rejectionHandled', (p: any) => {
-    unhandledRejections.delete(p);
-});
+console.log('Listen http://localhost:3000/');
