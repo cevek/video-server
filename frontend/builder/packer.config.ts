828b9a3ff22ecbb9d7c445a1780b392bb1afccf8
cody <cevek@ya.ru>
2016-11-16 23:36:17 +0500

    Add alias fast-react and copy font-awesome

--- frontend/builder/packer.config.ts
+++ frontend/builder/packer.config.ts
@@ -2,10 +2,19 @@ import {Packer, copy, combineJS, combineCSS} from "webbuilder";
 import {ts} from "webbuilder/dist/plugins/ts";
 import {sass} from "webbuilder/dist/plugins/sass";
 
-export default new Packer({dest: 'dist', context: __dirname + '/../src/'}, promise => promise
+const config = {
+    dest: 'dist',
+    context: __dirname + '/../src/',
+    alias: {
+        'react': 'fast-react',
+        'react-dom': 'fast-react',
+    }
+};
+export default new Packer(config, promise => promise
     .then(ts())
     .then(sass())
     .then(copy('index.html'))
+    .then(copy('font-awesome-4.6.1/fonts/*.woff', filename => filename.replace('font-awesome-4.6.1/', '')))
     .then(combineJS('app.js', 'bundle.js'))
     .then(combineCSS('style.css'))
 );
