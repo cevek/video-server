ea6e68dd19dd471e2e253713592002967a3fc2a7
cody <cevek@ya.ru>
2016-11-25 01:35:37 +0500

    Add utils fileExists, downloadFile

--- backend/utils.ts
+++ backend/utils.ts
@@ -1,7 +1,8 @@
 "use strict";
-
-import {exec as Exec, spawn as Spawn, ChildProcess} from 'child_process';
-export async function exec(exec:string, params?:{maxBuffer?: number, timeout?: number}) {
+import request = require('request');
+import {createWriteStream, accessSync, constants} from "fs";
+import {exec as Exec, spawn as Spawn, ChildProcess} from "child_process";
+export async function exec(exec: string, params?: {maxBuffer?: number, timeout?: number}) {
     if (!params) {
         params = {};
     }
@@ -10,29 +11,29 @@ export async function exec(exec:string, params?:{maxBuffer?: number, timeout?: n
     }
     return await (new Promise<string>((resolve, reject) => {
         Exec(exec, params,
-            (err:Error, stdout:Buffer, stderr:Buffer) =>
+            (err: Error, stdout: string, stderr: string) =>
                 err ? reject(exec + '\n' + err) : resolve(stdout.toString() + stderr.toString()));
     }));
 }
 
-export async function spawn(exec:string, callback?:(output:string, cp?:ChildProcess)=>void, timeout?:number) {
-    return await (new Promise((resolve, reject)=> {
+export async function spawn(exec: string, callback?: (output: string, cp?: ChildProcess)=>void, timeout?: number) {
+    return await (new Promise((resolve, reject) => {
         var parts = exec.split(' ');
         var cp = Spawn(parts.shift(), parts);
         var stdout = '';
-        var handler = (data:Buffer) => {
+        var handler = (data: Buffer) => {
             const str = data.toString();
             stdout += str;
             callback(str, cp);
         };
         cp.stdout.on('data', handler);
         cp.stderr.on('data', handler);
-        cp.on('error', (err:Error)=>reject(exec + '\n' + err));
-        cp.on('close', (code:number) => {
+        cp.on('error', (err: Error) => reject(exec + '\n' + err));
+        cp.on('close', (code: number) => {
             resolve(stdout);
         });
         if (timeout > 0) {
-            setTimeout(()=> {
+            setTimeout(() => {
                 reject();
                 cp.kill();
             }, timeout * 1000);
@@ -44,14 +45,32 @@ export function genId() {
     return +(Math.random().toString().substr(2, 6) + Math.random().toString().substr(2, 6) + Math.random().toString().substr(2, 6));
 }
 
-export function toMap<T>(items:T[], key?:string) {
+export function toMap<T>(items: T[], key?: string) {
     if (!key) {
         key = 'id';
     }
-    var map:{[key: string]: T} = {};
+    var map: {[key: string]: T} = {};
     for (var i = 0; i < items.length; i++) {
         var item = <any>items[i];
         map[item[key]] = item;
     }
     return map;
+}
+
+export function fileExists(filename: string) {
+    try {
+        accessSync(filename, constants.F_OK)
+        return true;
+    } catch (e) {
+        return false;
+    }
+}
+
+export function downloadFile(url: string, filename: string) {
+    return new Promise((resolve, reject) => {
+        const file = createWriteStream(filename);
+        request(url).pipe(file);
+        file.on('finish', resolve);
+        file.on('error', reject);
+    });
 }
